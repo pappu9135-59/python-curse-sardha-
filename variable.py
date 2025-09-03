6cd0cde8125706465d83dda3824b65151d6d4ed7
Your Name <your.email@example.com>
2025-09-03 10:39:28 +0530

    updated

---
+++ variable.py
@@ -0,0 +1,96 @@
+# name="pappu kumar"
+# age=12
+# price=25.99         
+# print("my name is :", name)
+# print("and my agfe is :",age)
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# airthmetic operateros 
+# a=4
+# b=51
+# print(a-b)
+# print(a+b)
+# print(a*b)
+# prin               t(a/b)
+# print(a**b)  #a^b matlab 
+
+
+#relational opratyers 
+# a=50
+# b=20
+# print(a==b) #false 
+
+# print(a!=b) # true 
+# print(a>=b) 
+# print(a<=b)
+# print(a<b)
+# print(a>b)
+
+#assingment opreaters 
+# num=10
+# num+=10
+# print(num)
+
+# logical oprerators 
+# a=501
+# b=30
+# print(not False)
+# print(not(a>b))  
+
+# val1=True
+# val2=True
+# print("and oprroters:", val1 and val2)
+
+#type nconversion 
+# a=2
+# b=4.25
+# sum=a+b  #sum2.0+4.25 => 6.25
+# name=input("enter your name:")   
+# print("welcome",name)
+    
+# val =input("enter som e value :")   
+# print(type(val),val)
+    
+# Name=input("enter name:")
+# age=input("enter age:")
+# marks=float(input("enter marks: "))
+
+# print("welcome",name)
+# print("age=",age )
+# print("marks=",marks)
+  
+
+#practice question  lactutre one  by sardha maam 
+
+
+1 #write a programn 2 numbers &print their sum 
+
+# a=25
+# b=20
+# print(a+b) this is rong 
+
+# frist=int(input ("enter the frist:"))
+# second=int(input("enter the second:"))
+# print ("sum=",frist+ second)
+
+2# wap to input side of a square &print its area 
+
+# side = float(input("enter the value"))
+# print("area=",side*side)
+
+3#  wap to input 2 floating point  number & print their average 
+
+# a=int(input ("enter the frist :"))
+# b=int(input("enter the second :"))
+# print("avg =",(a+b)/2)
+
+
+4# wap to input 2 int number ,,a and b 
+# print true if a is greater than or equal to b if not print false 
+
+
+# a=int(input ("enter the frist:"))
+# b=int(input ("enter the second :"))
+# print (a>=b)
